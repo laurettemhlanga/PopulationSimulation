4efbf97d99ca6915d172b348730a32984a1df99b
Mhlanga <laurette@sun.ac.za>
2019-02-07 11:39:17 +0200

    satisfactory calculation of infected individuals

--- R/Infected_population.R
+++ R/Infected_population.R
@@ -8,8 +8,7 @@
 #' @return returns an array of dimensions time, age and "time_since_infection"
 #' @examples
 #' Infected <- generate_infected_population_array(susceptible_pop_counts = susceptible_pop_counts,
-#'  incidence_prob = incidence_matrix,survival_probability = infected_survival_probs)
-#'
+#'  incidence_prob = incidence_m,survival_probability = infected_survival_probs)
 #'
 #'
 
@@ -19,38 +18,39 @@ generate_infected_population_array <- function(susceptible_pop_counts,
                                                survival_probability)
 {
 
-#adopt pevious matrix dimensions
-   infected_population_array <-  array(NA,
-                                       dim = c(dim(survival_probability)[1],
-                                               dim(survival_probability)[2],
-                                               (dim(survival_probability)[3]) + 1))
+  #adopt pevious matrix dimensions
+  infected_population_array <-  array(NA, dim = c(dim(survival_probability)[1],
+                                                  dim(survival_probability)[2],
+                                                  (dim(survival_probability)[3]) + 1))
 
-   infected_population_array[ , , 1] <- incidence_prob * susceptible_pop_counts
-
+  infected_population_array[ , , 1] <- incidence_prob * susceptible_pop_counts
 
 
-   for (times in 1:nrow(susceptible_pop_counts)){
 
-     for(aa in 1:ncol(susceptible_pop_counts)){
+  for (times in 1:nrow(susceptible_pop_counts)){
 
-       for (ta in 2:dim(infected_population_array)[3]){
+    for(aa in 1:ncol(susceptible_pop_counts)){
 
-         if (any(times - (ta ) <= 0 || aa - (ta ) <=  0)){
+      for (ta in 1:(dim(infected_population_array)[3] - 1)){
 
-           infected_population_array[times, aa ,ta ] <- 0
-
-         }else{
+        if (any(((times - ta ) <= 0 || (aa - ta ) <=  0)) ){
 
 
-           infected_population_array[times, aa ,ta ] <- incidence_matrix[times - (ta ), aa - (ta)] *
-             susceptible_pop_counts[times - (ta ), aa - (ta )] *
-             survival_probability[times - (ta ), aa - (ta ), (ta )]
+          infected_population_array[times, aa ,ta  + 1 ] <-  ifelse(is.na(susceptible_pop_counts[times , aa]) == T, NA , 0)
+          #ensures the entries that have NA by design are maintained
+
+        }else{
 
 
-         }
+          infected_population_array[times, aa ,ta + 1 ] <- incidence_prob[(times - ta), (aa - ta)] *
+            susceptible_pop_counts[(times - ta), (aa - ta )] *
+            survival_probability[(times - ta ), (aa - ta ), ta]
+
 
         }
-     }
+
+      }
+    }
   }
 
 
@@ -58,13 +58,21 @@ generate_infected_population_array <- function(susceptible_pop_counts,
 
 }
 
-
-
-
-
-Infected <- generate_infected_population_array(susceptible_pop_counts = susceptible_pop_counts,
+infected <- generate_infected_population_array(susceptible_pop_counts = susceptible_pop_counts,
                                                incidence_prob = incidence_m,
                                                survival_probability = infected_survival_probs)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
